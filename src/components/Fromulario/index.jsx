61a826d3fa0fe45d09d013ba9f7c563e0ef6e4af
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-03-26 20:47:49 +0000

    controlando estado do formulário

--- src/components/Fromulario/index.jsx
+++ src/components/Fromulario/index.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
+
 const Formulario = () => {
+
+  const [data, setData] = useState('yyyy-MM-dd')
+  const [periodo, setPeriodo] = useState('')
+  const [pSistole, setPSistole] = useState(0)
+  const [pDiastole, setPDiastole] = useState(0)
+
   return (
     <form>
       <label htmlFor="data">
         Data:
       </label>
-      <input type="date" name="data" id="data" />
+      <input type="date" name="data" id="data" 
+        value={data} onChange={(evento) => setData(evento.target.value)} 
+      />
       
       <label htmlFor="periodo-dia">
         Período:
       </label>
-      <select name="periodo-dia" id="periodo-dia">
+      <select name="periodo-dia" id="periodo-dia" 
+        onChange={(evento) => setPeriodo(evento.target.value)}
+      >
         <option value="">Selecione</option>
         <option value="manha">Manhã</option>
         <option value="tarde">Tarde</option>
@@ -19,12 +31,16 @@ const Formulario = () => {
       <label htmlFor="p-sistole">
         Pressão sistólica:
       </label>
-      <input type="number" name="p-sistole" id="p-sistole" />
+      <input type="number" name="p-sistole" id="p-sistole" min={0} max={200} step={5}
+        value={pSistole} onChange={(evento) => setPSistole(evento.target.value)} 
+      />
       
       <label htmlFor="p-diastole">
         Pressão diastólica:
       </label>
-      <input type="number" name="p-diastole" id="p-diastole" />
+      <input type="number" name="p-diastole" id="p-diastole" min={0} max={200} step={5} 
+        value={pDiastole} onChange={(evento) => setPDiastole(evento.target.value)}
+      />
       
       <button>Registrar</button>
     </form>
